9630f6be0b804004040581016bedae473032bd4c
ggarciaverdugo001 <60108556+ggarciaverdugo001@users.noreply.github.com>
2022-04-08 13:17:30 +0200

    Add files via upload

---
+++ P5 STA/src/main/java/pl/ProductBean.java
@@ -0,0 +1,14 @@
+package pl;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class ProductBean implements Serializable {
+
+	public ProductBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ P5 STA/src/main/java/pl/ShopBean.java
@@ -0,0 +1,12 @@
+package pl;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ShopBean {
+
+	public ShopBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
